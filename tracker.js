6cccdac05cbe5a3a2afe476cad81d9fdcc03d1fe
sri-rajan <sriraja781@gmail.com>
2026-02-06 02:09:05 +0530

    tracker updated

---
+++ tracker.js
@@ -0,0 +1,24 @@
+//DSA and Algo ------------------ everything try 3 times with 3 days gap, if you remember change to complete else try again
+
+// ARRAY  --> done
+
+// Linked List --> done tried-06-02-2026
+
+// stacks --> todo
+// queues --> todo
+// Hash Tables --> todo
+// Two Pointers  --> todo
+// sliding window --> todo
+// Recursion --> todo
+// backTracking --> todo
+// sorthing --> todo
+// searching --> todo
+// bit manipulation --> todo
+// Tree --> todo
+// Binery Search Trees --> todo
+// Trie --> todo
+// Heaps --> todo
+// Greedy --> todo
+// Dynamic Programming --> todo
+// Graphs --> todo
+// Union Find --> todo
