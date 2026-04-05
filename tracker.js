aedb97efad4853e5e2b872198cb7a751f12d2883
sri-rajan <sriraja781@gmail.com>
2026-04-05 16:18:21 +0530

    tracker updated

--- tracker.js
+++ tracker.js
@@ -1,14 +1,15 @@
 //DSA and Algo ------------------ everything try 3 times with 3 days gap, if you remember change to complete else try again
 
+// ------ BASIC ----
 // ARRAY  --> in progress
 // Linked List --> in progress
 // STACK --> todo
 // QUEUES --> todo
 // HASH TABLE --> todo
-
 // Two Pointers  --> in progress
 // sliding window --> in progress
 
+// ------- HARD -----
 // Recursion --> todo
 // backTracking --> todo
 // sorthing --> todo
